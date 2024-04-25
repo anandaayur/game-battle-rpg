8b104da4505ad1bd91ea28371be1451420f8419a
anandaayur <167510738+anandaayur@users.noreply.github.com>
2024-04-25 18:24:40 +0700

    Update Character.java

--- TUGAS GAME RPG PBO/Character.java
+++ TUGAS GAME RPG PBO/Character.java
@@ -5,38 +5,32 @@ class Character {
     protected int HP;
     protected int MP;
 
-    // Konstruktor untuk inisialisasi karakter
     public Character(String nama, String ras, int level) {
         this.nama = nama;
         this.ras = ras;
         this.level = level;
-        this.HP = level * 10; // Misalnya, HP maksimum adalah 100 * level
-        this.MP = level * 5; // Misalnya, MP maksimum adalah 50 * level
+        this.HP = level * 10; 
+        this.MP = level * 5;
     }
 
-    // Metode untuk menyerang karakter lain
     public void attack(Character target) {
         // Implementasi serangan karakter
     }
 
-    // Metode untuk menggunakan skill
     public void useSkill(Character target) {
         // Implementasi penggunaan skill
     }
 
-    // Metode untuk menggunakan item
-    public void useItem() {
+    public void useItem(Character target) {
         // Implementasi penggunaan item
     }
 
-    // Metode untuk meningkatkan level karakter
     public void levelUp() {
         this.level++;
-        this.HP = level * 100; // Mengembalikan HP ke maksimum
-        this.MP = level * 50; // Mengembalikan MP ke maksimum
+        this.HP = level * 100;
+        this.MP = level * 50;
     }
 
-    // Override metode toString untuk mencetak informasi karakter
     @Override
     public String toString() {
         return nama + ": HP = " + HP + ", MP = " + MP;
